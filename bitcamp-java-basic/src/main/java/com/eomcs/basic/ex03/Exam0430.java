134054f345c278a8e53aa928496fb57fea146c26
seoggwonlee <sysktho@gmail.com>
2020-07-22 18:02:12 +0900

    8일차 학습 PUSH

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0430.java
@@ -0,0 +1,27 @@
+// Exam0240.java
+// 주제 : 정수값을 메모리에 저장하는 방법
+
+package com.eomcs.basic.ex03;
+
+//# 문자 리터럴 III
+public class Exam0430 {
+
+    public static void main(String[] args) {
+      
+        System.out.println(0x0041);
+        System.out.println(0x41);
+        System.out.println((char)0x41);
+        System.out.println('A');
+        System.out.println((int)'A');
+        System.out.println('A'+1);
+        System.out.println((char)('A'+1));
+        System.out.println("----------------");
+        
+        for (int i = 'A'; i <= 'z'; i++)
+          System.out.println((char)i);
+        
+        
+        
+     }
+}
+
---
+++ mini-pms-practice/src/main/java/com/eomcs/pms/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner keyScan = new Scanner(System.in);
+        
+        System.out.print("A 입력 : ");
+        int A = keyScan.nextInt();
+        System.out.print("B 입력 : ");
+        int B = keyScan.nextInt();
+
+    if (A > 0 && B < 10) {
+        System.out.print("결과 : ");
+        System.out.println(A + B);
+    }
+    else {                
+            
+            System.out.println("다시 시도하십시오");
+    }
+
+    }
+}
