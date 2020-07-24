d1827ebf9f45a0cb41554ebdbcb249f05b46c120
seoggwonlee <sysktho@gmail.com>
2020-07-24 19:39:47 +0900

    배열 이용하여 변수 생성하기 까지

--- bitcamp-java-project/src/main/java/com/eomcs/pms/App2.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App2.java
@@ -7,17 +7,16 @@ public class App2 {
 
   public static void main(String[] args) {
     
-    int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0;
+    int[] num = new int[5];
     int count = 1;
     
-    String title1="", title2="", title3="", title4="", title5="";
-    String content1="", content2="", content3="", content4="", content5="";
-    Date startDate1=null, startDate2=null, startDate3=null, startDate4=null, startDate5=null;
-    Date endDate1=null, endDate2=null, endDate3=null, endDate4=null, endDate5=null;
-    String created1="", created2="", created3="", created4="", created5="";
-    String members1="", members2="", members3="", members4="", members5="";
+    String[] title = new String[5];
+    String[] content = new String[5];
+    Date[] startDate = new Date[5];
+    Date[] endDate = new Date[5];
+    String[] created = new String[5];
+    String[] members =new String[5];
     
-            
     Scanner KeyInput = new Scanner(System.in);
     
     System.out.print("[");
@@ -25,26 +24,26 @@ public class App2 {
     System.out.println("]");
     
     System.out.print("번호? ");
-    num1 = KeyInput.nextInt();
+    num[0] = KeyInput.nextInt();
     KeyInput.nextLine();
 
     System.out.print("프로젝트명: ");
-    title1= KeyInput.nextLine();
+    title[0] = KeyInput.nextLine();
     
     System.out.print("내용: ");
-    content1 = KeyInput.nextLine();
+    content[0] = KeyInput.nextLine();
     
     System.out.print("시작일: ");
-    startDate1 = Date.valueOf(KeyInput.nextLine()); 
+    startDate[0] = Date.valueOf(KeyInput.nextLine()); 
         
     System.out.print("종료일: ");
-    endDate1 = Date.valueOf(KeyInput.nextLine());
+    endDate[0] = Date.valueOf(KeyInput.nextLine());
 
     System.out.print("만든이: ");
-    created1 = KeyInput.nextLine();
+    created[0] = KeyInput.nextLine();
         
     System.out.print("팀원: ");
-    members1 = KeyInput.nextLine();
+    members[0] = KeyInput.nextLine();
     
     System.out.print("계속 입력하시겠습니까? (y/N)");
     String response = KeyInput.nextLine();
@@ -54,26 +53,26 @@ public class App2 {
       count++;
       
       System.out.print("번호? ");
-      num2 = KeyInput.nextInt();
+      num[1] = KeyInput.nextInt();
       KeyInput.nextLine();
 
       System.out.print("프로젝트명: ");
-      title2= KeyInput.nextLine();
+      title[1]= KeyInput.nextLine();
       
       System.out.print("내용: ");
-      content2 = KeyInput.nextLine();
+      content[1] = KeyInput.nextLine();
       
       System.out.print("시작일: ");
-      startDate2 = Date.valueOf(KeyInput.nextLine()); 
+      startDate[1] = Date.valueOf(KeyInput.nextLine()); 
           
       System.out.print("종료일: ");
-      endDate2 = Date.valueOf(KeyInput.nextLine());
+      endDate[1] = Date.valueOf(KeyInput.nextLine());
 
       System.out.print("만든이: ");
-      created2 = KeyInput.nextLine();
+      created[1] = KeyInput.nextLine();
           
       System.out.print("팀원: ");
-      members2 = KeyInput.nextLine();
+      members[1] = KeyInput.nextLine();
       
       System.out.print("계속 입력하시겠습니까? (y/N)");
       response = KeyInput.nextLine();
@@ -85,26 +84,26 @@ public class App2 {
           count++;
           
           System.out.print("번호? ");
-          num3 = KeyInput.nextInt();
+          num[2] = KeyInput.nextInt();
           KeyInput.nextLine();
 
           System.out.print("프로젝트명: ");
-          title3= KeyInput.nextLine();
+          title[2]= KeyInput.nextLine();
           
           System.out.print("내용: ");
-          content3 = KeyInput.nextLine();
+          content[2] = KeyInput.nextLine();
           
           System.out.print("시작일: ");
-          startDate3 = Date.valueOf(KeyInput.nextLine()); 
+          startDate[2] = Date.valueOf(KeyInput.nextLine()); 
               
           System.out.print("종료일: ");
-          endDate3 = Date.valueOf(KeyInput.nextLine());
+          endDate[2] = Date.valueOf(KeyInput.nextLine());
 
           System.out.print("만든이: ");
-          created3 = KeyInput.nextLine();
+          created[2] = KeyInput.nextLine();
               
           System.out.print("팀원: ");
-          members3 = KeyInput.nextLine();
+          members[2] = KeyInput.nextLine();
           
           System.out.print("계속 입력하시겠습니까? (y/N)");
           response = KeyInput.nextLine();
@@ -116,26 +115,26 @@ public class App2 {
             count++;
             
             System.out.print("번호? ");
-            num4 = KeyInput.nextInt();
+            num[3] = KeyInput.nextInt();
             KeyInput.nextLine();
 
             System.out.print("프로젝트명: ");
-            title4 = KeyInput.nextLine();
+            title[3] = KeyInput.nextLine();
             
             System.out.print("내용: ");
-            content4 = KeyInput.nextLine();
+            content[3] = KeyInput.nextLine();
             
             System.out.print("시작일: ");
-            startDate4 = Date.valueOf(KeyInput.nextLine()); 
+            startDate[3] = Date.valueOf(KeyInput.nextLine()); 
                 
             System.out.print("종료일: ");
-            endDate4 = Date.valueOf(KeyInput.nextLine());
+            endDate[3] = Date.valueOf(KeyInput.nextLine());
 
             System.out.print("만든이: ");
-            created4 = KeyInput.nextLine();
+            created[3] = KeyInput.nextLine();
                 
             System.out.print("팀원: ");
-            members4 = KeyInput.nextLine();
+            members[3] = KeyInput.nextLine();
             
             System.out.print("계속 입력하시겠습니까? (y/N)");
             response = KeyInput.nextLine();
@@ -147,26 +146,26 @@ public class App2 {
               count++;
               
               System.out.print("번호? ");
-              num5 = KeyInput.nextInt();
+              num[4] = KeyInput.nextInt();
               KeyInput.nextLine();
 
               System.out.print("프로젝트명: ");
-              title5= KeyInput.nextLine();
+              title[4]= KeyInput.nextLine();
               
               System.out.print("내용: ");
-              content5 = KeyInput.nextLine();
+              content[4] = KeyInput.nextLine();
               
               System.out.print("시작일: ");
-              startDate5 = Date.valueOf(KeyInput.nextLine()); 
+              startDate[4] = Date.valueOf(KeyInput.nextLine()); 
                   
               System.out.print("종료일: ");
-              endDate5 = Date.valueOf(KeyInput.nextLine());
+              endDate[4] = Date.valueOf(KeyInput.nextLine());
 
               System.out.print("만든이: ");
-              created5 = KeyInput.nextLine();
+              created[4] = KeyInput.nextLine();
                   
               System.out.print("팀원: ");
-              members5 = KeyInput.nextLine();
+              members[4] = KeyInput.nextLine();
               
               System.out.print("계속 입력하시겠습니까? (y/N)");
               response = KeyInput.nextLine();
@@ -182,15 +181,15 @@ public class App2 {
     System.out.println("--------------------------------------");
     
     
-      System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num1, title1, content1, startDate1.toString(), endDate1.toString(), created1, members1);
+      System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num[0], title[0], content[0], startDate[0].toString(), endDate[0].toString(), created[0], members[0]);
     if(count > 1) {
-    System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num2, title2, content2, startDate2.toString(), endDate2.toString(), created2, members2);
+    System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num[1], title[1], content[1], startDate[1].toString(), endDate[1].toString(), created[1], members[1]);
       if(count > 2) {
-    System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num3, title3, content3, startDate3.toString(), endDate3.toString(), created3, members3);
+    System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num[2], title[2], content[2], startDate[2].toString(), endDate[2].toString(), created[2], members[2]);
         if(count > 3) {    
-    System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num4, title4, content4, startDate4.toString(), endDate4.toString(), created4, members4);
+    System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num[3], title[3], content[3], startDate[3].toString(), endDate[3].toString(), created[3], members[3]);
           if(count > 4) {
-            System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num5, title5, content5, startDate5.toString(), endDate5.toString(), created5, members5);
+            System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", num[4], title[4], content[4], startDate[4].toString(), endDate[4].toString(), created[4], members[4]);
           }
         }
       }
