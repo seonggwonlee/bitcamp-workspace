e54bbed795b636b863994e352d5e503446d6f567
seoggwonlee <sysktho@gmail.com>
2020-07-28 20:40:05 +0900

    재PUSH

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/practice05/practice21.java
@@ -0,0 +1,10 @@
+package com.eomcs.basic.practice05;
+
+public class practice21 {
+
+  public static void main (String[] args) {
+    
+    int pixel = 0x003f4478;
+    System.out.println(pixel & 0x0000ffff);
+  }
+}
