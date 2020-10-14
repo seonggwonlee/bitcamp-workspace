12c8fd2fb80b3ef495302c7766cb66fd4a2142e2
seoggwonlee <sysktho@gmail.com>
2020-10-14 19:20:15 +0900

    10/14

--- bitcamp-java-project-client/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-java-project-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -4,27 +4,59 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import com.eomcs.util.Prompt;
 
 // stateful 통신
 // 서버와 연결하여 간단한 메시지 주고 받기
 
 public class ClientApp {
   public static void main(String[] args) {
-    // 서버주소 : localhost / 포트 : 8888
+    if (args.length != 2) {
+      System.out.println("프로그램 사용법 : ");
+      System.out.println("java -cp ... ClientApp 서버주소 포트번호");
+      System.exit(0);
+    }
 
-    // 서버와 연결 된 소켓을 생성한다.
-    // 소켓을 통해 문자열을 입출력할 수 있도록 스트림 객체 준비
-    // 먼저 서버에 간단한 인사말을 보낸다.
-    try (Socket socket = new Socket("localhost", 8888);
+    boolean stop = false;
+
+    try (Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
         PrintWriter out = new PrintWriter(socket.getOutputStream());
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-          out.println("Hello");
+
+      while (true) {
+          String input = Prompt.inputString("명령 > ");
+          out.println(input);
           out.flush();
 
-          String response = in.readLine();
-          System.out.println(response);
+          receiveResponse(in);
+
+          if (input.equalsIgnoreCase("quit")) {
+            break;
+          }
+            else if (input.equalsIgnoreCase("stop")) {
+              stop = true;
+              break;
+            }
+          }
         } catch (Exception e) {
           e.printStackTrace();
         }
+
+    if (stop) {
+      try (Socket socket = new Socket(args[0], Integer.parseInt(args[1]))) {
+
+        } catch (Exception e) {
+
+        }
+      }
     }
+
+  private static void receiveResponse(BufferedReader in) throws Exception {
+    while (true) {
+    String response = in.readLine();
+    if (response.length() == 0)
+      break;
+    System.out.println(response);
+    }
+  }
 }
